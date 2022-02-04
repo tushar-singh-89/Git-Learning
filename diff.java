5e2302e663a8fdb4adf0eb7b12ca43e430048123
tushar-singh-89 <tmanu.ts@gmail.com>
2022-02-04 20:21:49 +0530

    added mult.java

--- diff.java
+++ diff.java
@@ -1,6 +1,6 @@
 public class Diff {
     public static void main(String[] args) {
         int a = 5, b = 10;
-        System.out.println(a - b);
+        System.out.println("The difference is : " + a - b);
     }
 }
---
+++ mult.java
@@ -0,0 +1,6 @@
+public class Mult {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        System.out.println("The multiply is : " + a * b);
+    }
+}
