7b3b27da26d2acfd244a6a04b668884d95ac09ea
tushar-singh-89 <tmanu.ts@gmail.com>
2022-02-05 17:56:07 +0530

    modified diff.java

--- diff.java
+++ diff.java
@@ -1,4 +1,5 @@
 public class Diff {
+    // Adding a commnet to test
     public static void main(String[] args) {
         int a = 5, b = 10;
         System.out.println("The difference is : " + a - b);
